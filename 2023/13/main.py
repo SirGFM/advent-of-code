fb31bf3bfff15eab64a9f3a0a0ea79ef06ab8a8c
SirGFM <gfmandaji@hotmail.com>
2023-12-13 17:13:37 -0300

    2023: Add day 13 part 2

--- 2023/13/main.py
+++ 2023/13/main.py
@@ -2,12 +2,12 @@ import sys
 
 rows = []
 columns = []
-patterns = []
+str_patterns = []
 
 for line in sys.stdin:
 	line = line.strip()
 	if len(line) == 0:
-		patterns.append((rows, columns))
+		str_patterns.append((rows, columns))
 		rows = []
 		columns = []
 		continue
@@ -18,32 +18,59 @@ for line in sys.stdin:
 	rows.append(line)
 	for x, c in enumerate(line):
 		columns[x] += c
-patterns.append((rows, columns))
+str_patterns.append((rows, columns))
 
-def compare(arr, start):
+patterns = []
+for str_rows, str_columns in str_patterns:
+	rows = []
+	columns = []
+	for r in str_rows:
+		r = '0b' + r.replace('.', '0').replace('#', '1')
+		rows.append(int(r, 2))
+	for c in str_columns:
+		c = '0b' + c.replace('.', '0').replace('#', '1')
+		columns.append(int(c, 2))
+	patterns.append((rows, columns))
+
+def compare_part1(arr, start):
 	for i in range(start + 1):
 		if start + i + 1 >= len(arr):
 			return i != 0
-		elif arr[start - i] != arr[start + i + 1]:
+		elif (arr[start - i] ^ arr[start + i + 1]) != 0:
 			return False
 	return True
 
-def find(arr):
+def compare_part2(arr, start):
+	did_swap = False
+	for i in range(start + 1):
+		if start + i + 1 >= len(arr):
+			return did_swap
+
+		diff = arr[start - i] ^ arr[start + i + 1]
+		if diff != 0:
+			if not did_swap and diff.bit_count() == 1:
+				did_swap = True
+				continue
+			return False
+	return did_swap
+
+def find(arr, comparer):
 	for i in range(len(arr)):
-		if compare(arr, i):
+		if comparer(arr, i):
 			return i
 	return -1
 
-def part1():
+def compute(comparer):
 	total = 0
 	for rows, columns in patterns:
-		row = find(rows) + 1
+		row = find(rows, comparer) + 1
 		if row > 0:
 			total += row * 100
 			continue
-		column = find(columns) + 1
+		column = find(columns, comparer) + 1
 		total += column
 		continue
 	print(total)
 
-part1()
+compute(compare_part1)
+compute(compare_part2)
