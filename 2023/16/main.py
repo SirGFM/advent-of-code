df02268562d7b1073723aa534a38ebcaa4dce280
SirGFM <gfmandaji@hotmail.com>
2023-12-16 19:53:31 -0300

    2023: Add day 16 part 1

---
+++ 2023/16/main.py
@@ -0,0 +1,143 @@
+import enum
+import sys
+
+def debug(grid):
+	for line in grid:
+		dbg = ''
+		for node in line:
+			dbg += node.beam.value
+		print(dbg)
+	print()
+
+class Direction(enum.Enum):
+	NONE = '.'
+	UP = '^'
+	DOWN = 'v'
+	LEFT = '<'
+	RIGHT = '>'
+
+	def dx(self):
+		if self == Direction.LEFT:
+			return -1
+		elif self == Direction.RIGHT:
+			return 1
+		return 0
+
+	def dy(self):
+		if self == Direction.UP:
+			return -1
+		elif self == Direction.DOWN:
+			return 1
+		return 0
+
+class Mirror(enum.Enum):
+	NONE = '.'
+	VERTICAL = '|'
+	HORIZONTAL = '-'
+	RIGHT = '/'
+	LEFT = '\\'
+
+	def reflect(self, direction):
+		if self == Mirror.VERTICAL:
+			if direction in (Direction.UP, Direction.DOWN):
+				return [direction]
+			return [Direction.UP, Direction.DOWN]
+		elif self == Mirror.HORIZONTAL:
+			if direction in (Direction.RIGHT, Direction.LEFT):
+				return [direction]
+			return [Direction.RIGHT, Direction.LEFT]
+		elif self == Mirror.RIGHT:
+			if direction == Direction.UP:
+				return [Direction.RIGHT]
+			elif direction == Direction.RIGHT:
+				return [Direction.UP]
+			elif direction == Direction.DOWN:
+				return [Direction.LEFT]
+			elif direction == Direction.LEFT:
+				return [Direction.DOWN]
+		elif self == Mirror.LEFT:
+			if direction == Direction.UP:
+				return [Direction.LEFT]
+			elif direction == Direction.LEFT:
+				return [Direction.UP]
+			elif direction == Direction.DOWN:
+				return [Direction.RIGHT]
+			elif direction == Direction.RIGHT:
+				return [Direction.DOWN]
+		return [direction]
+
+class Node:
+	def __init__(self, c, x, y):
+		self.beam = Direction.NONE
+		self.mirror = Mirror(c)
+		self.x = x
+		self.y = y
+		self.exec = set()
+
+	def run(self, grid):
+		self.send_beam(self.beam, grid)
+
+	def send_beam(self, direction, grid):
+		if direction == Direction.NONE:
+			return
+
+		while True:
+			x = self.x + direction.dx()
+			y = self.y + direction.dy()
+			if x < 0 or x >= len(grid[0]) or y < 0 or y >= len(grid):
+				return
+
+			next_node = grid[y][x]
+			beams = next_node.mirror.reflect(direction)
+			if len(beams) > 1:
+				next_node.run_beams(beams, grid)
+				return
+			elif beams[0] in next_node.exec:
+				return
+			else:
+				direction = beams[0]
+				next_node.exec.add(direction)
+				next_node.set_beam(direction)
+				self = next_node
+
+	def run_beams(self, beams, grid):
+		for beam in beams:
+			if beam not in self.exec:
+				self.send_beam(beam, grid)
+				self.set_beam(beam)
+
+	def set_beam(self, beam):
+		self.exec.add(beam)
+
+		# Keep every 'down' beam so it may be executed on the next step.
+		if Direction.DOWN in self.exec:
+			self.beam = Direction.DOWN
+		else:
+			self.beam = beam
+
+grid = []
+for y, line in enumerate(sys.stdin):
+	line = line.strip()
+	if len(line) == 0:
+		continue
+
+	grid_line = []
+	for x, c in enumerate(line):
+		grid_line.append(Node(c, x, y))
+	grid.append(grid_line)
+
+	if y > 0:
+		for node in grid[y - 1]:
+			if node.beam == Direction.DOWN:
+				node.run(grid)
+	else:
+		starter = Node('.', -1, 0)
+		starter.beam = Direction.RIGHT
+		starter.run(grid)
+
+total = 0
+for line in grid:
+	for node in line:
+		if node.beam != Direction.NONE:
+			total += 1
+print(total)
