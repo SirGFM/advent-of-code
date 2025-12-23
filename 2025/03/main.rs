c7e02b99066d636207676c726bc7995e77c7ce7e
SirGFM <gfmandaji@hotmail.com>
2025-12-23 12:38:58 -0300

    2025: Add day 3 part 2

--- 2025/03/main.rs
+++ 2025/03/main.rs
@@ -6,6 +6,7 @@ fn main() {
 
 	let mut buf = String::new();
 	let mut result_1: u64 = 0;
+	let mut result_2: u64 = 0;
 
 	loop {
 		let num = handle.read_line(&mut buf).unwrap();
@@ -14,25 +15,52 @@ fn main() {
 		}
 		let line = buf.trim().as_bytes();
 
-		let mut first: u64 = 0;
-		let mut second: u64 = 0;
-
-		for i in 0..line.len() {
-			let digit = (line[i] - b'0') as u64;
-
-			if i != line.len() - 1 && digit > first {
-				first = digit;
-				second = 0;
-			} else if digit > second {
-				second = digit;
-			}
-		}
-
-		let found = first * 10 + second;
-		result_1 += found;
+		result_1 += calculate(&line, 2);
+		result_2 += calculate(&line, 12);
 
 		buf.clear();
 	}
 
 	println!("part 1: {}", result_1);
+	println!("part 2: {}", result_2);
+}
+
+fn calculate(line: &[u8], num_digits: usize) -> u64 {
+	let mut digits: Vec<usize> = Vec::<usize>::new();
+
+	for i in 0..num_digits {
+		digits.push(line.len() - (num_digits - 1 - i));
+	}
+
+	get_largest(&line, &mut digits, 0);
+	return to_number(&line, &digits);
+}
+
+fn get_largest(line: &[u8], digits: &mut[usize], end: usize) {
+	let mut i = digits[0];
+
+	while i > end {
+		let digit = line[digits[0] - 1];
+		let cur = line[i - 1];
+
+		if cur >= digit {
+			digits[0] = i;
+
+			if digits.len() > 1 {
+				get_largest(&line, &mut digits[1..], i);
+			}
+		}
+
+		i -= 1;
+	}
+}
+
+fn to_number(line: &[u8], digits: &[usize]) -> u64 {
+	let mut ret: u64 = 0;
+
+	for i in digits {
+		ret = ret * 10 + ((line[i - 1] - b'0') as u64);
+	}
+
+	return ret;
 }
