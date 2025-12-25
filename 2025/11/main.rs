fc6b10c99d26a07d3332e28fcc2f1fca61e85915
SirGFM <gfmandaji@hotmail.com>
2025-12-25 08:17:42 -0300

    2025: Add day 11 part 2

--- 2025/11/main.rs
+++ 2025/11/main.rs
@@ -7,7 +7,8 @@ fn main() {
 	let mut handle = stdin.lock();
 
 	let mut buf = String::new();
-	let mut result_1: u64 = 0;
+	let result_1: u64;
+	let mut result_2: u64;
 
 	let mut map_to = HashMap::<u32, Vec<u32>>::new();
 
@@ -34,8 +35,33 @@ fn main() {
 		buf.clear();
 	}
 
-	let start = str2code("you");
-	let target = str2code("out");
+	let you = str2code("you");
+	let out = str2code("out");
+
+	if map_to.get(&you).is_some() {
+		result_1 = count_paths(you, out, &map_to);
+	} else {
+		result_1 = 0;
+	}
+
+	let svr = str2code("svr");
+	let fft = str2code("fft");
+	let dac = str2code("dac");
+
+	if map_to.get(&svr).is_some() {
+		result_2 = count_paths(svr, fft, &map_to);
+		result_2 *= count_paths(fft, dac, &map_to);
+		result_2 *= count_paths(dac, out, &map_to);
+	} else {
+		result_2 = 0;
+	}
+
+	println!("part 1: {}", result_1);
+	println!("part 2: {}", result_2);
+}
+
+fn count_paths(start: u32, target: u32, map_to: &HashMap<u32, Vec<u32>>) -> u64 {
+	let mut result = 0;
 
 	let mut nodes = HashMap::<u32, u64>::new();
 	nodes.insert(start, 1);
@@ -46,11 +72,15 @@ fn main() {
 		for (node, count) in &nodes {
 			let count = *count;
 
-			for neighbour in map_to.get(&node).unwrap() {
+			let neighbourhood = match map_to.get(&node) {
+				Some(nodes) => nodes,
+				None => continue,
+			};
+			for neighbour in neighbourhood {
 				let neighbour = *neighbour;
 
 				if neighbour == target {
-					result_1 += count;
+					result += count;
 				} else {
 					let new_count = match next.get(&neighbour) {
 						Some(old) => old + count,
@@ -64,7 +94,7 @@ fn main() {
 		nodes = next;
 	}
 
-	println!("part 1: {}", result_1);
+	return result;
 }
 
 fn str2code(value: &str) -> u32 {
